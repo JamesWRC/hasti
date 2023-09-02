d918df5d8fd796a7fbcd88f3e953b3b4417a995d
JamesWRC <James.cockshott@pm.me>
2023-09-02 19:58:41 +1000

    adding local support

--- backend/buildImage.sh
+++ backend/buildImage.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-docker buildx build --platform linux/arm64 -t jameswrc/hastibackend --push . --no-cache
+docker buildx build --platform linux/amd64,linux/arm64 -t jameswrc/hastibackend --push . --no-cache
