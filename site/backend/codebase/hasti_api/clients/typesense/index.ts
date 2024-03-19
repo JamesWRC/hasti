1cff118cd33ff3bbb0c592185831bf13ebdaaf01
JamesWRC <James.cockshott@pm.me>
2024-03-19 21:40:40 +1100

    added example .envs

--- site/backend/codebase/hasti_api/clients/typesense/index.ts
+++ site/backend/codebase/hasti_api/clients/typesense/index.ts
@@ -10,7 +10,7 @@ let tsClient = new TypesenseSearchClient({
     'port': 8108,      // For Typesense Cloud use 443
     'protocol': 'http'   // For Typesense Cloud use https
   }],
-  'apiKey': '6itSfL674zTHi1kkzJgzPlKnAKuXnRDJE7grndLSLWzPbWcn1W',
+  'apiKey': process.env.TYPESENSE_API_KEY as string,
   'connectionTimeoutSeconds': 5
 })
 
