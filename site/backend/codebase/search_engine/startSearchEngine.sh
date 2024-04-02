c91f3c1f918a0cc0a4f2605f0eaf4e66f7415d9f
JamesWRC <James.cockshott@pm.me>
2024-04-02 14:37:35 +1100

    spelling

--- site/backend/codebase/search_engine/startSearchEngine.sh
+++ site/backend/codebase/search_engine/startSearchEngine.sh
@@ -2,13 +2,13 @@
 read -p "Enter region ('develop' or 'production'): " region
 
 if [ $region = "develop" ]; then
-    echo "Stopping development environment 'HAST_DEV'..."
+    echo "Starting development environment 'HAST_DEV'..."
     sleep 5
     docker compose -p 'HAST_DEV' up -d
 fi
 
 if [ $region = "production" ]; then
-    echo "Stopping production environment 'HAST_PROD'..."
+    echo "Starting production environment 'HAST_PROD'..."
     sleep 5
     docker compose -p 'HAST_PROD' up -d
 fi
