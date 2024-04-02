df66d198473aa3f5cd648ce1182623c8b7b50242
JamesWRC <James.cockshott@pm.me>
2024-04-02 14:33:33 +1100

    fizing vars

--- site/backend/codebase/search_engine/stopSearchEngine.sh
+++ site/backend/codebase/search_engine/stopSearchEngine.sh
@@ -1,12 +1,13 @@
+#!/usr/bin/env bash
 read -p "Enter region ('develop' or 'production'): " region
 
-if [ $1 = "develop" ]; then
+if [ $region = "develop" ]; then
     echo "Stopping development environment 'HAST_DEV'..."
     sleep 5
     docker compose -p 'HAST_DEV' down -d
 fi
 
-if [ $1 = "production" ]; then
+if [ $region = "production" ]; then
     echo "Stopping production environment 'HAST_PROD'..."
     sleep 5
     docker compose -p 'HAST_PROD' down -d
