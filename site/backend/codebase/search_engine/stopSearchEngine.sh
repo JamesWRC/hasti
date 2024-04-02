7ad9de3066bfe4c0ebadb4fb3316ceaeb4529582
JamesWRC <James.cockshott@pm.me>
2024-04-02 14:38:11 +1100

    fixing command

--- site/backend/codebase/search_engine/stopSearchEngine.sh
+++ site/backend/codebase/search_engine/stopSearchEngine.sh
@@ -4,11 +4,11 @@ read -p "Enter region ('develop' or 'production'): " region
 if [ $region = "develop" ]; then
     echo "Stopping development environment 'HAST_DEV'..."
     sleep 5
-    docker compose -p 'HAST_DEV' down -d
+    docker compose -p 'HAST_DEV' down
 fi
 
 if [ $region = "production" ]; then
     echo "Stopping production environment 'HAST_PROD'..."
     sleep 5
-    docker compose -p 'HAST_PROD' down -d
+    docker compose -p 'HAST_PROD' down
 fi
