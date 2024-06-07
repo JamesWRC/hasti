aea9f351c58eba0d78aecf3d0ea4dbb06eac132a
JamesWRC <38713144+JamesWRC@users.noreply.github.com>
2024-06-07 22:31:12 +1000

    Update Search.tsx

--- src/components/ui/search/Search.tsx
+++ src/components/ui/search/Search.tsx
@@ -1,11 +1,17 @@
+'use client'
 
-
+import { ChevronDoubleDownIcon } from '@heroicons/react/24/solid';
 // Make a search Bar component
 
+import { Box, Button, Collapse, Group, Text } from '@mantine/core';
+import { useDisclosure } from "@mantine/hooks";
+
 export default function Search() {
+    const [opened, { toggle }] = useDisclosure(false);
 
     return (
-        <form className="max-w-md mx-auto pt-8">
+        <>
+        <form className="max-w-md mx-auto pt-3">
             <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div className="relative">
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -13,9 +19,22 @@ export default function Search() {
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                     </svg>
                 </div>
-                <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-2xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                <input type="search" id="default-search" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-2xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-200 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Integrations, Themes etc..." required />
                 {/* <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> */}
             </div>
         </form>
+            <Box className='w-full mr-16'>
+            <Group justify="center" mb={5}>
+              <Button onClick={toggle} className='text-dark'><ChevronDoubleDownIcon className='text-dark h-4 w-4'/></Button>
+            </Group>
+      
+            <Collapse in={opened}>
+              <div className='mx-16'>
+                content
+              </div>
+            </Collapse>
+          </Box>
+          </>
+
     )
 }
