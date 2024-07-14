6eb5bdd8e8fbe6b4d90d6f2c20bd72df8b393c28
JamesWRC <James.cockshott@pm.me>
2024-07-14 22:23:21 +1000

    fixes

--- backend/stopHastiServices.sh
+++ backend/stopHastiServices.sh
@@ -2,13 +2,13 @@
 read -p "Enter region ('develop' or 'production'): " region
 
 if [ $region = "develop" ]; then
-    echo "Starting development environment 'HAST_DEV'..."
+    echo "Stopping development environment 'HAST_DEV'..."
     sleep 5
     docker compose -p 'HASTI_DEV' -f docker-compose.dev.yml down
 fi
 
 if [ $region = "production" ]; then
-    echo "Starting production environment 'HAST_PROD'..."
+    echo "Stopping production environment 'HAST_PROD'..."
     sleep 5
     docker compose -p 'HAST_PROD' down
 fi
