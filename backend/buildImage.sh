46253d7d31636feabc1fe5995afff22199748a83
JamesWRC <James.cockshott@pm.me>
2024-07-15 23:12:44 +1000

    Update buildImage.sh

--- backend/buildImage.sh
+++ backend/buildImage.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 # docker buildx build --platform linux/amd64,linux/arm64 -t jameswrc/hastibackend --push . --no-cache
-docker build -t jameswrc/hastibackend --push . --no-cache
+docker build --no-cache -t jameswrc/hastibackend --push .
