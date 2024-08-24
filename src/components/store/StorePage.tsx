01b1c653f6a8f625f648f8d61b3cf5d9a350f0bd
JamesWRC <James.cockshott@pm.me>
2024-08-24 12:33:23 +1000

    Fixed home styling, rm store page search type filtering

--- src/components/store/StorePage.tsx
+++ src/components/store/StorePage.tsx
@@ -12,9 +12,10 @@ export default function StorePage() {
     <div className="bg-white py-24 sm:py-28">
       <div className="mx-auto max-w-[150%] px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
-          <Search />
-
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl pt-8">Store</h2>
+          <div className='pb-8 text-left'>
+            <Search />
+          </div>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Store</h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
             Explore popular Home Assistant Themes and Integrations
           </p>
